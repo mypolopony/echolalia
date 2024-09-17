6a5e4f428ddbd0d7e28b1cfbacd5d9fe42c5b7f5
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2024-09-17 07:24:45 -0700

    Add median difference to _utils in order to facilitate time diff'

--- echolalia/_utils.py
+++ echolalia/_utils.py
@@ -2,6 +2,8 @@ from typing import Any, TypedDict
 from datetime import datetime
 
 import boto3
+import pandas as pd
+import numpy as np
 from io import BytesIO
 
 
@@ -47,12 +49,14 @@ def get_matching_s3_objects(bucket, prefix="", search="", suffix="") -> S3Result
     if isinstance(prefix, str):
         kwargs["Prefix"] = prefix
 
+    # Pagination
     for page in paginator.paginate(**kwargs):
         try:
             contents = page["Contents"]
         except KeyError:
             break
 
+        # Check for matching objects
         for obj in contents:
             key = obj["Key"]
             if search in key and key.endswith(suffix):
@@ -94,3 +98,27 @@ def read_s3_file(bucket: str, key: str) -> str:
     text_content = content.decode('utf-8')
 
     return text_content
+
+
+def median_diff(series: pd.Series) -> np.float64:
+    """
+    This function calculates the median difference between consecutive elements in a pandas Series. It
+    improves on the native impelementation in that it can handle series of size 1.
+
+    Parameters
+    ----------
+    series : pd.Series
+        The input series for which to calculate the median difference.
+    
+    Returns
+    -------
+    float
+        The median difference between consecutive elements in the series.
+
+    """
+    if len(series) > 1:                 # Normal diff median calculation
+        return series.diff().median()
+    elif len(series) == 1:              # Median is just the singular value
+        return series[0]
+    else:                               # No values to calculate
+        return None
