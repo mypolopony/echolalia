a39264db37d1f1709bdf36a947f5ed929f709d20
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2024-09-17 01:34:48 -0700

    Remove parsing rules;

--- echolalia/__init__.py
+++ echolalia/__init__.py
@@ -5,13 +5,15 @@ __version__ = "0.0.1"
 from ._utils import get_matching_s3_objects
 from .parser import WhatsAppParser
 from .contextualizer import Contextualizer
+# from .cleasing_rules import CHATGPT_CLEANSING_RULES
 
 __all__ = [
     # Utility stuff
     "get_matching_s3_objects"
-    ,
+
     # Parser stuff
     "WhatsAppParser"
+    # "CHATGPT_CLEANSING_RULES"
     
     # Contextualizer stuff
     "Contextualizer"
