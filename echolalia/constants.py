bc3fb35ad8e40f1b770b461333acc6256caba020
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2024-10-16 23:24:16 -0700

    new ARN

--- echolalia/constants.py
+++ echolalia/constants.py
@@ -1,4 +1,3 @@
 # S3 Details
 S3_BUCKET_NAME = "smcphers-echolalia"
-# SAGEMAKER_ARN = "arn:aws:iam::897729117324:role/service-role/AmazonSageMaker-ExecutionRole-20241013T222797"
 SAGEMAKER_ARN = "arn:aws:iam::897729117324:role/SageMakerExecutionRole"
