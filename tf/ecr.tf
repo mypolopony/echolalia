b82fd6dbd97bc4d5197acf3bdfd2c5e2fe320674
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2024-10-22 16:43:21 -0700

    add chat ecr

--- tf/ecr.tf
+++ tf/ecr.tf
@@ -8,3 +8,14 @@ resource "aws_ecr_repository" "echolalia_ecr_repo" {
     scan_on_push = false
   }
 }
+
+# The repo for storing images
+resource "aws_ecr_repository" "echolalia_chat_ecr_repo" {
+  name                 = "echolalia-chat"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
