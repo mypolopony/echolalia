9dcaa2441aefc898592a9064c4082a0de254e0bd
Selwyn-Lloyd McPherson <selwyn.mcpherson@gmail.com>
2024-10-23 14:55:53 -0700

    add constant for chat image

--- echolalia/constants.py
+++ echolalia/constants.py
@@ -6,3 +6,4 @@ SAGEMAKER_ARN = "arn:aws:iam::897729117324:role/SageMakerExecutionRole"
 
 # ECR Image
 IMAGE_URL = "897729117324.dkr.ecr.us-west-1.amazonaws.com/echolalia:latest"
+CHAT_IMAGE_URL = "897729117324.dkr.ecr.us-west-1.amazonaws.com/echolalia-chat:latest"
